f51dbdeb0424f34a6545ff8a2d9d6028233f10e4
Mike Nesci <mikenesci@hotmail.com>
2022-03-18 22:23:32 -0400

    Add today's code wars challenge

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function areYouPlayingBanjo(name) {
     // assign a variable to the first letter of name input, but always in lower case
     let firstLetterOfName = name[0].toLowerCase();
@@ -7,4 +8,19 @@ function areYouPlayingBanjo(name) {
     } else {
         return `${name} does not play banjo`;
     }
+}
+
+
+function countBy(x, n) {
+    // assign an empty array for the results
+    let result = [];
+    // iterate over a loop from 1 to n*x (the first n multiples of x must be within n*x)
+    for(let i = 1; i <= n*x; i++) {
+        // if number is divisible by x, it is a multiple of x
+        if (i % x === 0) {
+            result.push(i);
+        }
+    }
+    // return result
+    return result;
 }
