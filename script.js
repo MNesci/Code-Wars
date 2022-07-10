3a1579bf19ba26c449aff480667616b06cfe6f0d
Mike Nesci <mikenesci@hotmail.com>
2022-07-10 12:38:54 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -1830,4 +1830,8 @@ function lovefunc(flower1, flower2){
     return (flower1 % 2 === 0 && flower2 % 2 !== 0) || (flower1 % 2 !== 0 && flower2 % 2 === 0);
 };
 
+function greet(name){
+    return `Hello, ${name} how are you doing today?`;
+};
+
 */
