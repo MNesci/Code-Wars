88b5d4167291a57ee9e0e3a9b404bcd6ced08405
Mike Nesci <mikenesci@hotmail.com>
2022-07-11 09:35:08 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -1834,4 +1834,44 @@ function greet(name){
     return `Hello, ${name} how are you doing today?`;
 };
 
+function alphabetPosition(text) {
+  // create an object to convert letters to numbers
+  let alphabetKey = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+  // create an array of the characters in the string
+  let arrayOfCharacters = text.split('');
+  // filter the array to only have letters
+  let arrayOfLetters = arrayOfCharacters.filter((character) => alphabetKey[character.toLowerCase()]);
+  // map the letters to their corresponding numbers
+  let arrayOfNumbers = arrayOfLetters.map((letter) => alphabetKey[letter.toLowerCase()])
+  // return the numbers in a string
+  return arrayOfNumbers.join(' ');
+};
+
 */
