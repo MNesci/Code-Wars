bc99a67a0d14e04b4e200ade0c663c1c69753ef3
Mike Nesci <mikenesci@hotmail.com>
2022-07-12 07:30:17 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -1874,4 +1874,12 @@ function alphabetPosition(text) {
   return arrayOfNumbers.join(' ');
 };
 
+function between(a, b) {
+    let array = [];
+    for (let i = a; i <= b; i++) {
+        array.push(i);
+    };
+    return array;
+};
+
 */
