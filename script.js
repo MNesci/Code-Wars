2cdc0f096281c397ad68b71c4db9ea9c09066d76
Mike Nesci <mikenesci@hotmail.com>
2022-07-13 07:43:29 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -1882,4 +1882,14 @@ function between(a, b) {
     return array;
 };
 
+function getRealFloor(n) {
+    if (n <= 0) {
+      return n;
+    } else if (n <= 12) {
+      return n - 1;
+    } else {
+      return n - 2;
+    };
+};
+
 */
