769bf75b6718f407b4d62d6898d07282d0e5bd68
Mike Nesci <mikenesci@hotmail.com>
2022-07-23 21:40:20 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -1954,4 +1954,52 @@ function grow(x){
     return x.reduce((product, integer) => product * integer, 1);
 };
 
+function high(x){
+    // create an array of words
+    let arrayOfWords = x.split(' ');
+    // create an object of letter scores
+    let letterScores = {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4,
+      e: 5,
+      f: 6,
+      g: 7,
+      h: 8,
+      i: 9,
+      j: 10,
+      k: 11,
+      l: 12,
+      m: 13,
+      n: 14,
+      o: 15,
+      p: 16,
+      q: 17,
+      r: 18,
+      s: 19,
+      t: 20,
+      u: 21,
+      v: 22,
+      w: 23,
+      x: 24,
+      y: 25,
+      z: 26,
+    };
+    // iterate over the array
+    let highestScore = arrayOfWords.reduce((currentScore, word) => {
+  
+        let wordScore = { word: word, score: 0};
+        let arrayOfLetters = word.split('');
+        arrayOfNumbers = arrayOfLetters.map((letter) => {
+            return letterScores[letter];
+        });
+        wordScore.score = arrayOfNumbers.reduce((totalScore, letterScore) => {
+            return totalScore + letterScore;
+        }, 0);
+        return wordScore.score > currentScore.score ? wordScore : currentScore;
+    }, { word: 'a', score: 1});
+    return highestScore.word;
+};
+
 */
