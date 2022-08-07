f853364732c4c1c88b8df8eb92ff3753f2ea07da
Mike Nesci <mikenesci@hotmail.com>
2022-08-06 20:53:48 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -2153,4 +2153,8 @@ function findNeedle(haystack) {
     return `found the needle at position ${haystack.indexOf('needle')}`;
 };
 
+function createPhoneNumber(numbers){
+    return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
+};
+
 */
