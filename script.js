7d961a6eedbb323c50bbb7d6c9e724c294a1657d
Mike Nesci <mikenesci@hotmail.com>
2022-08-14 11:47:48 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -2218,4 +2218,8 @@ function smash (words) {
    return words.join(' ');
 };
 
+function getMiddle(s) {
+    return s.length % 2 === 0 ? s.substring(s.length / 2 - 1, s.length / 2 + 1) : s.substring((s.length + 1) / 2 - 1, (s.length + 1) / 2);
+}
+
 */
