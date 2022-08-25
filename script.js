67019e3b8d7207dd300e8229ace694ee6c29b0d8
Mike Nesci <mikenesci@hotmail.com>
2022-08-25 09:18:33 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -2337,4 +2337,30 @@ String.prototype.toAlternatingCase = function () {
     return correctCaseArray.join('');
 };
 
+function howMuchILoveYou(nbPetals) {
+    while(nbPetals > 6) {
+        nbPetals -= 6;
+    };
+    switch (nbPetals) {
+        case 1:
+            return "I love you";
+            break;
+        case 2:
+            return "a little";
+            break;
+        case 3:
+            return "a lot";
+            break;
+        case 4:
+            return "passionately";
+            break;
+        case 5:
+            return "madly";
+            break;
+        case 6:
+            return "not at all";
+            break;
+    };
+};
+
 */
