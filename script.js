c7732c117d826ebce48c2f74acc1548b9a2c8ca8
Mike Nesci <mikenesci@hotmail.com>
2022-09-07 11:40:54 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -2556,5 +2556,17 @@ function capitals(word) {
     return arrayOfCapitalIndexes;
 };
 
+function peopleWithAgeDrink(old) {
+    if (old < 14) {
+        return 'drink toddy';
+    } else if (old < 18) {
+        return 'drink coke';
+    } else if (old < 21) {
+        return 'drink beer';
+    } else {
+        return 'drink whisky';
+    };
+};
+
 */
 
