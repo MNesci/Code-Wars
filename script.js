d76b7d5f019306a2ef1a02156cd4f470939cd1e1
Mike Nesci <mikenesci@hotmail.com>
2022-09-08 08:00:44 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -2568,5 +2568,13 @@ function peopleWithAgeDrink(old) {
     };
 };
 
+function summation(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++) {
+        sum += i;
+    };
+    return sum;
+};
+
 */
 
