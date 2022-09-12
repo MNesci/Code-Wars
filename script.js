4628e1261eb75aeb2444849b87e70969a0deac4f
Mike Nesci <mikenesci@hotmail.com>
2022-09-12 17:08:22 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -2596,5 +2596,16 @@ function dutyFree(normPrice, discount, hol){
     };
 };
 
+function updateLight(current) {
+    switch (current) {
+        case 'green' :
+            return 'yellow';
+        case 'yellow' :
+            return 'red';
+        case 'red':
+            return 'green';
+    };
+};
+
 */
 
