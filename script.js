5233a854c6a2a5e8616c058f245554b62f517f4f
Mike Nesci <mikenesci@hotmail.com>
2022-09-20 14:35:21 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -2677,6 +2677,10 @@ function narcissistic(value) {
     return total === value;
 };
 
+function findUniq(arr) {
+    arr.sort((a, b) => a - b);
+    return arr[0] === arr[1] ? arr[arr.length - 1] : arr[0];
+};
 
 */
 
