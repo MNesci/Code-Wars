b97e08248b30ea61d793f628c1b8c602e208ea5a
Mike Nesci <mikenesci@hotmail.com>
2022-10-09 22:06:03 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -2995,4 +2995,14 @@ function duplicateEncode(word) {
     return encodedArray.join('');
 };
 
+function multipleOfIndex(array) {
+    let arrayToReturn = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % i === 0) {
+            arrayToReturn.push(array[i]);
+        };
+    };
+    return arrayToReturn;
+};
+
 */
