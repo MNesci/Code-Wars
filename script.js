8ed6cf1b7252a0adb06d03632dfe41a70f5d3d12
Mike Nesci <mikenesci@hotmail.com>
2022-10-10 16:19:55 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -3005,4 +3005,8 @@ function multipleOfIndex(array) {
     return arrayToReturn;
 };
 
+function feast(beast, dish) {
+    return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length -1] ? true : false;
+};
+
 */
