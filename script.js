4298a36e159f87b81e58ef143ba2d2e30dc9b77e
Mike Nesci <mikenesci@hotmail.com>
2022-10-18 20:21:11 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -3217,4 +3217,45 @@ function move (position, roll) {
     return position + roll * 2;
 };
 
+function decrypt(encryption) {
+    let alphabetCountTracker = {
+        a: 0,
+        b: 0,
+        c: 0,
+        d: 0,
+        e: 0,
+        f: 0,
+        g: 0,
+        h: 0,
+        i: 0,
+        j: 0,
+        k: 0,
+        l: 0,
+        m: 0,
+        n: 0,
+        o: 0,
+        p: 0,
+        q: 0,
+        r: 0,
+        s: 0,
+        t: 0,
+        u: 0,
+        v: 0,
+        w: 0,
+        x: 0,
+        y: 0,
+        z: 0,
+    };
+    for (let i = 0; i < encryption.length; i++) {
+        if (encryption[i] in alphabetCountTracker) {
+            alphabetCountTracker[encryption[i]] += 1;
+        } ; 
+    };
+    let decryption = '';
+    for (let letter in alphabetCountTracker) {
+        decryption += alphabetCountTracker[letter];
+    };
+    return decryption;
+};
+
 */
