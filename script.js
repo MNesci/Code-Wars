0ddfa5b5213f1373fa55a80a2e0222dac95807e2
Mike Nesci <mikenesci@hotmail.com>
2022-10-23 13:08:47 -0400

    Add today's codewars challenge

--- script.js
+++ script.js
@@ -3328,4 +3328,17 @@ function toWeirdCase(string) {
     return arrayOfWords.join(' ');
 };
 
+function inArray(array1,array2){
+    let arrayToReturn = [];
+   for (let array of array1) {
+        for (let i = 0; i < array2.length; i++) {
+            if (array2[i].includes(array)) {
+                arrayToReturn.push(array);
+                break;
+            };
+        };
+    };
+    return arrayToReturn.sort();
+};
+
 */
